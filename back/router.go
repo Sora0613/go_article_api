84ff2cbf740b30de07939d3a3d4e2259c243608d
Sora0613 <sora0613.contact@gmail.com>
2024-03-27 17:07:08 +0900

    fix. 該当の会社が見つからない場合の例外処理を追加。

--- back/router.go
+++ back/router.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"example/go/Models"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 	"net/http"
 	"time"
 )
@@ -20,9 +22,18 @@ func getCompany(c *gin.Context) {
 	db := gormConnect()
 	id := c.Param("id")
 	var company Models.Company
-	db.First(&company, id)
 
-	c.JSON(200, company)
+	// 会社がない場合。
+	if err := db.First(&company, id).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Company not found"})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+		return
+	}
+
+	c.JSON(http.StatusOK, company)
 }
 
 func postCompany(c *gin.Context) {
