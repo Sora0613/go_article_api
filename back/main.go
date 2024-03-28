a732c1e498a971012c44bd3b8e0889ae6a0c023c
Sora0613 <sora0613.contact@gmail.com>
2024-03-28 18:38:50 +0900

    a few fix.

--- back/main.go
+++ back/main.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"go_api/Database"
 	"go_api/Routes"
 )
 
 func main() {
-	Database.GormConnect()
-
+	// Database.GormConnect()
 	s := gin.Default()
-
 	Routes.SetupRoutes(s)
 
 	s.Run(":8080")
