bffd19fcd1cc41a36074d3f337d61080611e8518
Sora0613 <sora0613.contact@gmail.com>
2024-03-29 17:42:53 +0900

    upd. Modelの中身の変更をこなった。
    DB設計を見直して、Modelを変更した。
    各TableにArticleIDを配置し、テーブルの関係を調整した。

--- back/Models/Articles.go
+++ back/Models/Articles.go
@@ -3,9 +3,13 @@ package Models
 import "gorm.io/gorm"
 
 type Article struct {
-	gorm.Model `json:"gorm_._model"`
+	gorm.Model
 
-	Title      string `json:"title"`
-	CompanyID  uint   `json:"company_id"`   // get Company's info from this ID
-	OBVisitsID uint   `json:"ob_visits_id"` // get OBVisits's info from this ID
+	Title             Title             `json:"title"`
+	Company           Company           `json:"company"`
+	OBVisits          OBVisits          `json:"ob_visits"`
+	Offer             Offer             `json:"offer"`
+	InterviewFeedback InterviewFeedback `json:"interview_feedback"`
 }
+
+// SelectionProcess  SelectionProcess  `json:"selection_process"`
