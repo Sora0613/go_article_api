5d8303148097bf0b2b5bbde77d4c588c5a436374
Sora0613 <sora0613.contact@gmail.com>
2024-03-30 18:37:08 +0900

    upd. メモ書いた。

--- back/Models/OBVisits.go
+++ back/Models/OBVisits.go
@@ -5,5 +5,5 @@ import "gorm.io/gorm"
 type OBVisits struct {
 	gorm.Model
 	ArticleID uint   `gorm:"foreignKey:ArticleID" json:"article_id"`
-	Visited   string `json:"visited"`
+	Visited   string `json:"visited"` // Boolでも良いかも
 }
