d318dc4b64cd8a177b6391b6dd14cc5c0cdd1477
Sora0613 <sora0613.contact@gmail.com>
2024-04-08 18:30:41 +0900

    upd. main.goのdbに関する変更。 mainが走った段階でdbの接続を行い、Routesに渡すようにした。

--- back/main.go
+++ back/main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"go_api/Database"
 	"go_api/Routes"
 )
 
 func main() {
-	// Database.GormConnect()
+	db := Database.GormConnect()
 	s := gin.Default()
 
-	Routes.SetupRoutes(s)
+	Routes.SetupRoutes(s, db)
 
 	s.Run(":8080")
 }
